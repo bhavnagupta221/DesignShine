3d54256a4c108feeab9be71a7d211aae8f0c714b
Sarah Hall <hallsarahc97@gmail.com>
2018-11-07 20:56:00 -0800

    finished design of create new user page, upload picture button not functional

--- js/login.js
+++ js/login.js
@@ -3,12 +3,35 @@ $( "p" ).click(function() {
     console.log( "You clicked a paragraph!" );
 });
 
+//changes color of login in button
 $('#login').mousemove(function() {
 	$(this).css("background-color", "#57c658");
 	$(this).css("border-color", "#57c658");
 });
 
 $('#login').mouseleave(function() {
+	$(this).css("background-color","#343a40");
+	$(this).css("border-color", "#343a40");
+});
+
+//changes color of submit button, create account
+$('#submit').mousemove(function() {
+	$(this).css("background-color", "#57c658");
+	$(this).css("border-color", "#57c658");
+});
+
+$('#submit').mouseleave(function() {
+	$(this).css("background-color","#343a40");
+	$(this).css("border-color", "#343a40");
+});
+
+//changes color of back button, create account
+$('#back').mousemove(function() {
+	$(this).css("background-color", "#c40916");
+	$(this).css("border-color", "#c40916");
+});
+
+$('#back').mouseleave(function() {
 	$(this).css("background-color","#343a40");
 	$(this).css("border-color", "#343a40");
 });
