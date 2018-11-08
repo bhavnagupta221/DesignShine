fd13fe65f1f0fc5868cccdf7601c6b8fff25e5c4
Sarah Hall <hallsarahc97@gmail.com>
2018-11-07 19:13:44 -0800

    completed functionality of login page

--- js/login.js
+++ js/login.js
@@ -1,4 +1,14 @@
-// Event setup using a convenience method
+//test the link between files
 $( "p" ).click(function() {
     console.log( "You clicked a paragraph!" );
+});
+
+$('#login').mousemove(function() {
+	$(this).css("background-color", "#57c658");
+	$(this).css("border-color", "#57c658");
+});
+
+$('#login').mouseleave(function() {
+	$(this).css("background-color","#343a40");
+	$(this).css("border-color", "#343a40");
 });
