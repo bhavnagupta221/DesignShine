52c4502919b74d72e5c77d3ddb095f50858bc50a
Bhavna Gupta <bhavnagupta221@gmail.com>
2018-11-08 21:19:08 -0800

    we like templates

--- js/data.js
+++ js/data.js
@@ -1,28 +1,30 @@
-var eggs = {'title': 'Eggs',
-                  'seller': 'Jerrica',
-                  'type': 'produce',
-                  'description': '1 dozen fresh eggs',
-                  'expiration date': '11/21/18',
-                  'allergens': 'egg',
-                  'image': '../images/eggs.jpeg',
-                  'start-time': '6:00PM',
-                  'end-time': '6:50PM',
-                  'location': 'Price Center',
-                  'next': 'Broccoli'};
+var eggs = {'title': 'Eggs', 'seller': 'Jerrica', 'type': 'produce',
+            'description': '1 dozen fresh eggs', 'expiration date': '11/21/18',
+            'allergens': 'egg', 'image': '../images/eggs.jpeg',
+            'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center',
+            'next': 'Broccoli'};
 
-var broccoli = {'title': 'Broccoli',
-                  'seller': 'Jerrica',
-                  'type': 'produce',
-                  'description': 'yum yum broccoli',
-                  'expiration date': '11/28/18',
-                  'allergens': 'N/A',
-                  'image': '../images/broccoli.jpg',
-                  'start-time': '6:00PM',
-                  'end-time': '6:50PM',
-                  'location': 'Price Center',
-                  'next': 'Eggs'};
+var broccoli = {'title': 'Broccoli', 'seller': 'Jerrica', 'type': 'produce',
+                'description': 'yum yum broccoli', 'expiration date': '11/28/18',
+                'allergens': 'N/A', 'image': '../images/broccoli.jpg',
+                'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center',
+                'next': 'Brownies'};
 
-var complexData = [eggs, broccoli];
+var brownies = {'title': 'Brownies', 'seller': 'Jerrica', 'type': 'prepared', 'description': 'homemade supreme triple chunk brownies', 'expiration date': '11/12/18', 'allergens': 'eggs, nuts', 'image': '../images/brownies.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Pasta'};
+
+var pasta = {'title': 'Pasta', 'seller': 'Jerrica', 'type': 'packaged', 'description': 'uncooked box of pasta', 'expiration date': '1/1/22', 'allergens': 'gluten', 'image': '../images/pasta.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Banana'};
+
+var banana = {'title': 'Banana', 'seller': 'Jerrica', 'type': 'produce', 'description': '1 singular banana', 'expiration date': '11/12/18', 'allergens': 'N/A', 'image': '../images/banana.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Pistachios'};
+
+var pistachios = {'title': 'Pistachios', 'seller': 'Jerrica', 'type': 'packaged', 'description': 'bag of pistachios', 'expiration date': '10/1/19', 'allergens': 'nuts', 'image': '../images/pistachios.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Cheerios'};
+
+var cheerios = {'title': 'Cheerios', 'seller': 'Jerrica', 'type': 'packaged', 'description': '1 box of Cheerios', 'expiration date': '1/1/19', 'allergens': 'gluten', 'image': '../images/cheerios.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Fruit Loops'};
+
+var fruitloops = {'title': 'Fruit Loops', 'seller': 'Jerrica', 'type': 'packaged', 'description': '1 box of Fruit Loops', 'expiration date': '1/1/19', 'allergens': 'gluten', 'image': '../images/frootloops.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Done'};
+
+var done = {'title': 'Done'};
+
+var complexData = [eggs, broccoli, brownies, pasta, banana, pistachios, cheerios, fruitloops];
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -45,8 +47,8 @@ $(document).ready(function() {
     var curData = complexData[i];
     var curHtml = template(curData);
     parentDiv.append(curHtml);
-  }
-  */
+  }*/
+  
   // END - STEP 1
 
 
@@ -78,6 +80,11 @@ $(document).ready(function() {
   console.log('query for', projectTitle);
   // to get this to work like in class, comment out the "STEP 1" parts
   // above between BEGIN and END.
+
+  if (projectTitle == "Done") {
+    window.location.href = '../html/noMatch.html';
+  }
+  
   for (var i = 0; i < complexData.length; i++) {
     var curData = complexData[i];
     if (curData.title == projectTitle) {
