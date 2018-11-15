20ca21085deb1da236bae709ae22804a123eb581
Bhavna Gupta <bhavnagupta221@gmail.com>
2018-11-14 21:06:22 -0800

    idk

--- js/dropoff.js
+++ js/dropoff.js
@@ -12,15 +12,6 @@ var tempItem = {
   "location": ""*/
 };
 
-$("#dropofftype-submit").click(function() {
-  currItem = localStorage.key(1);
-  tempItem = JSON.parse(localStorage[currItem]);
-  tempItem.produce = document.getElementById('produce').checked;
-  tempItem.packaged = document.getElementById('packaged').checked;
-  tempItem.prepared = document.getElementById('prepared').checked;
-  localStorage.setItem(currItem, JSON.stringify(tempItem));
-});
-
 $("#dropoffinfo-submit").click(function() {
   tempItem.name = document.getElementById('name').value;
   tempItem.description = document.getElementById('desc').value;
@@ -28,6 +19,25 @@ $("#dropoffinfo-submit").click(function() {
   tempItem.allergens = document.getElementById('allergens').value;
   
   localStorage.setItem(tempItem.name, JSON.stringify(tempItem));
+});
+
+
+$("#dropofftype-submit").click(function() {
+  currItem = localStorage.key(0);
+  tempItem = JSON.parse(localStorage[currItem]);
+  tempItem.produce = document.getElementById('produce').checked;
+  tempItem.packaged = document.getElementById('packaged').checked;
+  tempItem.prepared = document.getElementById('prepared').checked;
+  localStorage.setItem(currItem, JSON.stringify(tempItem));
+});
+
+
+$("dropofftime-submit").click(function() {
+  currItem = localStorage.key(0);
+  tempItem = JSON.parse(localStorage[currItem]);
+  
+  localStorage.setItem(currItem, JSON.stringify(tempItem));
+  
 });
 
 $(document).ready(function() {
