09ef21c569aadfcbd152eccc405861bf1c6e9bb4
Bhavna Gupta <bhavnagupta221@gmail.com>
2018-11-14 23:48:27 -0800

    pls stop music

--- js/dropoff.js
+++ js/dropoff.js
@@ -11,6 +11,40 @@ var tempItem = {
   "endTime": "",
   "location": ""*/
 };
+var myimg = "";
+
+/*function encodeImageAsURL() {
+  console.log("hi!");
+  var file = document.getElementById("uploadpic").files[0];
+  var filereader = new FileReader();
+  filereader.onload = function(fileLoadedEvent) {
+    var srcData = fileLoadedEvent.target.result;
+    //var newImage = document.createElement('img');
+    //newImage.src = srcData;
+
+    //document.getElementById
+  }
+  myimg = filereader.readAsDataURL(file);
+};*/
+
+var uploadpic = document.getElementById("uploadpic");
+
+
+/*input.onchange = function() {
+  console.log("storing");
+  
+  var imgCanvas = document.createElement("canvas"),
+      imgContext = imgCanvas.getContext("2d");
+  
+  imgCanvas.width = uploadpic.width;
+  imgCanvas.height = uploadpic.height;
+  
+  imgContext.drawImage(uploadpic,0,0,uploadpic.width,uploadpic.height);
+  
+  myimg = imgCanvas.toDataURL("image/png");
+  
+});*/
+
 
 $("#dropoffinfo-submit").click(function() {
   tempItem.name = document.getElementById('name').value;
@@ -18,13 +52,16 @@ $("#dropoffinfo-submit").click(function() {
   tempItem.expirationDate = document.getElementById('exp-date').value;
   tempItem.allergens = document.getElementById('allergens').value;
   
+  //tempItem.image = myimg;
+  localStorage.setItem("currItem", tempItem.name);
   localStorage.setItem(tempItem.name, JSON.stringify(tempItem));
 });
 
 
 $("#dropofftype-submit").click(function() {
-  currItem = localStorage.key(0);
-  tempItem = JSON.parse(localStorage[currItem]);
+  currItem = localStorage.getItem("currItem");
+  tempItem = JSON.parse(localStorage.getItem(currItem));
+  
   tempItem.produce = document.getElementById('produce').checked;
   tempItem.packaged = document.getElementById('packaged').checked;
   tempItem.prepared = document.getElementById('prepared').checked;
@@ -33,13 +70,20 @@ $("#dropofftype-submit").click(function() {
 
 
 $("#dropofftime-submit").click(function() {
-  currItem = localStorage.key(0);
-  tempItem = JSON.parse(localStorage[currItem]);
+  currItem = localStorage.getItem("currItem");
+  tempItem = JSON.parse(localStorage.getItem(currItem));
+  tempItem.startTime = (document.getElementById('starthour').value + ":" + 
+                        document.getElementById('startmin').value +
+                        document.getElementById('startday-night').value);
+  tempItem.endTime = (document.getElementById('endhour').value + ":" +
+                      document.getElementById('endmin').value +
+                      document.getElementById('endday-night').value);
   var e = document.getElementById("location");
   tempItem.location = e.options[e.selectedIndex].value;
   localStorage.setItem(currItem, JSON.stringify(tempItem));
   
 });
+
 
 $(document).ready(function() {
   //console.log(tempItem);  
