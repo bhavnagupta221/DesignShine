058c0f13e45acf030ed0bd4db629bcc586bef109
Bhavna Gupta <bhavnagupta221@gmail.com>
2018-11-15 23:29:32 -0800

    sorry

--- js/data.js
+++ js/data.js
@@ -1,30 +1,3 @@
-var eggs = {'name': 'Eggs', 'packaged': 'false', 'produce': 'true', 'prepared': 'false',
-            'description': '1 dozen fresh eggs', 'expiration date': '11/21/18',
-            'allergens': 'egg', 'image': '../images/eggs.jpeg',
-            'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center',
-            'next': 'Brownies'};
-
-var broccoli = {'name': 'Broccoli', 'packaged': 'false', 'produce': 'true', 'prepared': 'false',
-                'description': 'yum yum broccoli', 'expiration date': '11/28/18',
-                'allergens': 'N/A', 'image': '../images/broccoli.jpg',
-                'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center',
-                'next': 'Brownies'};
-
-var brownies = {'name': 'Brownies', 'packaged': 'false', 'produce': 'false', 'prepared': 'true', 'description': 'homemade supreme triple chunk brownies', 'expiration date': '11/12/18', 'allergens': 'eggs, nuts', 'image': '../images/brownies.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Pasta'};
-
-var pasta = {'name': 'Pasta', 'packaged': 'true', 'produce': 'false', 'prepared': 'false', 'description': 'uncooked box of pasta', 'expiration date': '1/1/22', 'allergens': 'gluten', 'image': '../images/pasta.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Banana'};
-
-var banana = {'name': 'Banana', 'packaged': 'false', 'produce': 'true', 'prepared': 'false', 'description': '1 singular banana', 'expiration date': '11/12/18', 'allergens': 'N/A', 'image': '../images/banana.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Pistachios'};
-
-var pistachios = {'name': 'Pistachios', 'packaged': 'true', 'produce': 'false', 'prepared': 'false', 'description': 'bag of pistachios', 'expiration date': '10/1/19', 'allergens': 'nuts', 'image': '../images/pistachios.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Cheerios'};
-
-var cheerios = {'name': 'Cheerios', 'packaged': 'true', 'produce': 'false', 'prepared': 'false', 'description': '1 box of Cheerios', 'expiration date': '1/1/19', 'allergens': 'gluten', 'image': '../images/cheerios.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Fruit Loops'};
-
-var fruitloops = {'name': 'Fruit Loops', 'packaged': 'true', 'produce': 'false', 'prepared': 'false',  'description': '1 box of Fruit Loops', 'expiration date': '1/1/19', 'allergens': 'gluten', 'image': '../images/frootloops.jpg', 'start-time': '6:00PM', 'end-time': '6:50PM', 'location': 'Price Center', 'next': 'Done'};
-
-
-var complexData = [eggs, broccoli, brownies, pasta, banana, pistachios, cheerios, fruitloops];
-
 
 $("#dislike").click(function() {
   var counter = parseInt(localStorage.getItem("counter"));
