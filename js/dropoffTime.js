cac87ab07ddfa39de3b6e05a31352a8236d177b3
Elisha Beebe <elishabeebe@Elishas-MacBook-Pro.local>
2018-12-06 18:20:28 -0800

    time null

--- js/dropoffTime.js
+++ js/dropoffTime.js
@@ -12,7 +12,7 @@ $("#dropofftime-submit").click(function() {
   var e = document.getElementById("location");
   var location = e.options[e.selectedIndex].value;
 
-  if (startTime == "null:nullnull" || endTime == "null:nullnull" || location == "blank"){
+  if (startTime.includes("null") || endTime.includes("null") || location == "blank"){
     alert("Please fill out all the fields");
     return false;
   }
@@ -32,12 +32,12 @@ $(document).ready(function() {
   currItem = (localStorage.getItem("currItem"));
   if(currItem != "") {
     tempItem = JSON.parse(localStorage.getItem(currItem));
-    if(tempItem.startTime) {
+    if(tempItem.startTime && !tempItem.startTime.includes("null")) {
       document.getElementById('starthour').value = tempItem.startTime.slice(0,tempItem.startTime.indexOf(":"));
       document.getElementById('startmin').value = tempItem.startTime.slice(2,4);
       document.getElementById('startday-night').value = tempItem.startTime.slice(4,6);
     }
-    if(tempItem.endTime) {
+    if(tempItem.endTime && !tempItem.startTime.includes("null")) {
       document.getElementById('endhour').value = tempItem.endTime.slice(0,tempItem.endTime.indexOf(":"));
       document.getElementById('endmin').value = tempItem.endTime.slice(2,4);
       document.getElementById('endday-night').value = tempItem.endTime.slice(4,6);
@@ -45,7 +45,7 @@ $(document).ready(function() {
     if(tempItem.location) {
       document.getElementById("location").value = tempItem.location;
     }
-    
+
   }
   //console.log(tempItem);
   //console.log(localStorage.key(1));
